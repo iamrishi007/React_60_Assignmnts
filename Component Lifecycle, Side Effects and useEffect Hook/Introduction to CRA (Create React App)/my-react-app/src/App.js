bb476aa4f50913da03c1889e935a7e4714187ed0
Rushikesh Gaware <rushigaware1020@gmail.com>
2024-05-13 01:03:57 +0530

    counter app

---
+++ Component Lifecycle, Side Effects and useEffect Hook/Introduction to CRA (Create React App)/my-react-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+      <div style={{ margin: "auto 10px", padding: "10px", fontSize:"25px"}}>
+        <h1 style={{ margin: "auto 10px", padding: "10px" }}>{count}</h1>
+        <button style={{ margin: "auto 10px", padding: "10px" }} onClick={() => setCount(count + 1)}>+</button>
+        <button style={{ margin: "auto 10px", padding: "10px" }} onClick={() => setCount(count - 1)}>-</button>
+      </div>
+    </>
+  )
+}
+
+export default App;
